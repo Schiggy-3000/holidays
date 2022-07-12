6595c9016c19c1b444ca699e28d6cbf31688bcce
Schiggy-3000 <93184896+Schiggy-3000@users.noreply.github.com>
2022-07-12 11:14:42 +0200

    Function eda_holidays()

--- R/eda_holidays.R
+++ R/eda_holidays.R
@@ -5,28 +5,23 @@
 #'             holidays_2020_to_2025_long, holidays_2020_to_2025_long_major_regions
 #'             and holidays_2020_to_2025_long_switzerland. Not that XX is a
 #'             placeholder for 20, 21, 22, 23, 24 or 25.
-#' @param years The years for which you want the EDA to be executed for.
-#'              E.g. if you chose dataset holidays_2020_long, set years=2020.
-#'              Similarly, if you chose holidays_2020_to_2025_long, you can
-#'              set multiple years=c(2020,2021,2025).
 #'
 #' @return An exploratory data analysis for the input dataset.
+#' @import plotly
 #' @export
 #'
 #' @examples
-#' df <- data("holidays_2020_dates")
+#' df <- data("holidays_2020_long")
 #' df <- get(df)
-#' y <- c(2020, 2021, 2025)
-#' eda_holidays(data=df, years=y)
+#' eda_holidays(data=df)
 #'
 #'
 #'
-eda_holidays <- function(data, years) {
+eda_holidays <- function(data) {
 
 
-  # Setup
+  # Only include relevant years
   df <- data
-  y <- years
 
 
   # Plots for 'Plots' pane
@@ -35,6 +30,7 @@ eda_holidays <- function(data, years) {
 
 
   # Explanatory message
+  message("")
   message("")
   message("")
   message("Following an overview of all variables as well as their range of values.")
@@ -50,14 +46,12 @@ eda_holidays <- function(data, years) {
 
     if (is.character(df[,col])) {
 
-      message("")
       message(colnames(df)[col])
       message(toString(unique(df[,col])))
       message("")
 
     } else if (is.Date(df[,col]) || is.numeric(df[,col])) {
 
-      message("")
       message(colnames(df)[col])
       message(paste(range(df[,col])[1], range(df[,col])[2], sep=" ... "))
       message("")
@@ -69,5 +63,59 @@ eda_holidays <- function(data, years) {
     }
 
   }
+
+
+  # Interactive heat map
+  df.1 <- df
+
+  # Add new column with primary key
+  # This key is needed to plot the y-axis of the heatmap
+  # holidays_20XX_long:               primary.key -> 'Kanton' + 'Schule'
+  # holidays_20XX_long_major_region:  primary.key -> 'Grossregion'
+  # holidays_20XX_long_switzerland:   primary.key -> 'Land'
+  if (is.character(df.1$Kanton)) {
+
+    df.1$Primary.key <- with(df.1, paste0(Kanton, ", ", Schule))
+
+  } else if (is.character(df.1$Grossregion)) {
+
+    df.1$Primary.key <- df.1$Grossregion
+
+  } else if (is.character(df.1$Land)) {
+
+    df.1$Primary.key <- df.1$Land
+
+  } else {
+
+    print("Unknown dataset format")
+
+  }
+
+  # Create heat map
+  n <- length(unique(df.1$Primary.key))
+  heatmap <- plot_ly() %>%
+              add_trace(
+                type = "heatmap",
+                data = df.1,
+                x = ~Datum,
+                y = ~Primary.key,
+                z = ~Ferientag,
+                showscale = FALSE,
+                colors = c("slategray1", "steelblue3"),
+                hoverinfo = "text",
+                text = ~paste("Datum:",
+                              df.1$Datum,
+                              "<br>Ort:",
+                              df.1$Primary.key,
+                              "<br>Ferientag:",
+                              df.1$Ferientag)) %>%
+              layout(
+                xaxis = list(title=""),
+                yaxis = list(title="", tickvals=0:n))
+
+  # Plot heat map
+  heatmap
+
+
 
 }
